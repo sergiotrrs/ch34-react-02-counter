baef558cb573e70a9aae4bab7ebefe04d73ca26b
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-30 12:49:52 -0600

    ci: iterar de un arreglo de productos

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,10 +1,43 @@
+import { Box, Typography } from "@mui/material";
 import CounterApp from "../../components/counter/CounterApp"
+
+const products = [
+  {
+    sku: 1234,
+    title: "Produc1",
+    img: "www.image.com",
+    price: 23.56,
+  },
+  {
+    sku: 1235,
+    title: "Produc2",
+    img: "www.image.com",
+    price: 56.56,
+  },
+  {
+    sku: 1236,
+    title: "Produc3",
+    img: "www.image.com",
+    price: 3333.56,
+  },
+  {
+    sku: 1240,
+    title: "Produc4",
+    img: "www.image.com",
+    price: 789.56,
+  },
+];
 
 export const Products = () => {
   return (
     <div>
-      <CounterApp />
-    
+      {  products.map( ({title, img, price, sku}, index)=> 
+        <Box key={sku}>
+          <Typography variant="h3" >{title}</Typography>
+          <Typography variant="h4" >{price}</Typography>
+          <CounterApp />
+        </Box> 
+        )}
     </div>
   )
 }
