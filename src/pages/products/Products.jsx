600ae857495305987f70e773e30d3b741cc9fdb0
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-30 13:14:32 -0600

    ci: solicitud fetch en useEffect

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,10 @@
-import { Box, Typography } from "@mui/material";
-import CounterApp from "../../components/counter/CounterApp"
+import { Box, Container, Typography, Grid } from "@mui/material";
+import CounterApp from "../../components/counter/CounterApp";
+import { useEffect, useState } from "react";
 
-const products = [
+const url = "https://fakestoreapi.com/products";
+let products;
+/* const products = [
   {
     sku: 1234,
     title: "Produc1",
@@ -27,17 +30,35 @@ const products = [
     price: 789.56,
   },
 ];
-
+ */
 export const Products = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch(url);
+      products = await response.json();
+      setLoading(false);
+      console.log(products);
+    }
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      {  products.map( ({title, img, price, sku}, index)=> 
-        <Box key={sku}>
-          <Typography variant="h3" >{title}</Typography>
-          <Typography variant="h4" >{price}</Typography>
-          <CounterApp />
-        </Box> 
+    <Container maxWidth="md">
+      <Grid container spacing={2}>
+        {loading ? (
+          <Typography variant="h1">cargando...</Typography>
+        ) : (
+          products.map(({ title, img, price, id }, index) => (
+            <Grid key={id} xs={12} md={6} lg={3}>
+              <Typography variant="h3">{title}</Typography>
+              <Typography variant="h4">{price}</Typography>
+              <CounterApp />
+            </Grid>
+          ))
         )}
-    </div>
-  )
-}
+      </Grid>
+    </Container>
+  );
+};
