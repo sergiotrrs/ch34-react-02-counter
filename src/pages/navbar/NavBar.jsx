3eb3c4b06b15985631531783157bec0ef6ec6f39
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-30 11:00:11 -0600

    ci: agregando Lik al navBar

--- src/pages/navbar/NavBar.jsx
+++ src/pages/navbar/NavBar.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom"
 
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about-us">AboutUs</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>                    
+        </ul>
+      </nav>
+    </div>
   )
 }
